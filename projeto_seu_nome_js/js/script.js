36760382db2408443fd2c32b2aa6355b653b92f1
dier12421 <161588493+dier12421@users.noreply.github.com>
2024-04-23 08:54:18 -0300

    Atualizar o script.js

--- projeto_seu_nome_js/js/script.js
+++ projeto_seu_nome_js/js/script.js
@@ -13,19 +13,19 @@ function somar() {
 
   //Captura o que está dentro do primeiro input no formulário
   //Number - Transforma o texto capturado em Numero
-  let primeiro_numero_inteiro = Number(document.getElementById("primeiro_numero_inteiro").value);
+  let Quilômetro rodado(km)= Number(document.getElementById("Quilômetro rodado(km)").value);
 
   //Captura o que está dentro do segundo input no formulário
   //Number - Transforma o texto capturado em Numero
-  let segundo_numero_inteiro = Number(document.getElementById("segundo_numero_inteiro").value);
+  let Litro de combustível gastado(L)= Number(document.getElementById("Litro de combustível gastado(L)").value);
 
-  //Realiza a soma do valor que está na variável "primeiro_numero_inteiro" com o valor que está na
-  //variável "segundo_numero_inteiro"
+  //Realiza a soma do valor que está na variável "Quilômetro rodado(km)" com o valor que está na
+  //variável "Litro de combustível gastado(L)"
   //E a variável "resultado_soma" recebe o valor da soma
-  let resultado_soma = primeiro_numero_inteiro / segundo_numero_inteiro;
+  let resultado_soma = Quilômetro rodado(km)/ Litro de combustível gastado(L);
 
-  //Aqui vai escrever no input "resultado_soma" o valor que está armazenado na variável "resultado_soma"
-  document.getElementById("resultado_soma").value = resultado_soma;
+  //Aqui vai escrever no input "Total (km/L)" o valor que está armazenado na variável "Total (km/L)"
+  document.getElementById("resultado_soma").value = Total (km/L);
 
 
 }
